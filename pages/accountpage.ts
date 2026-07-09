32df6fce1bb18f12e8d2b1b920f6931f144193c8
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-09 17:15:24 +0530

    Added account creation feature

---
+++ pages/accountpage.ts
@@ -0,0 +1,46 @@
+/*
+
+Test Flow
+Open website
+
+↓
+
+Click Register
+
+↓
+
+Enter
+
+First Name
+
+Last Name
+
+Address
+
+City
+
+State
+
+Zip Code
+
+Phone
+
+SSN
+
+Username
+
+Password
+
+Confirm Password
+
+↓
+
+Click Register
+
+↓
+
+Verify
+
+"Your account was created successfully"
+
+*/
